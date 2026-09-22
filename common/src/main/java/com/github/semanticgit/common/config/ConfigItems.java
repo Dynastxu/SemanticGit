9978d0b67779fb520c98ca0a14817cd83485aa3b
dynastxu <151742029+ETOofficial@users.noreply.github.com>
2026-09-22 17:09:03 +0800

    添加浮点数配置生成器

--- common/src/main/java/com/github/semanticgit/common/config/ConfigItems.java
+++ common/src/main/java/com/github/semanticgit/common/config/ConfigItems.java
@@ -4,6 +4,7 @@ public final class ConfigItems {
     private static final RangeConfigItem.RangeConfigItemBuilder<Integer> INT = RangeConfigItem.<Integer>builder();
     private static final RangeConfigItem.RangeConfigItemBuilder<Long> LONG = RangeConfigItem.<Long>builder();
     private static final RangeConfigItem.RangeConfigItemBuilder<Double> DOUBLE = RangeConfigItem.<Double>builder();
+    private static final RangeConfigItem.RangeConfigItemBuilder<Float> FLOAT = RangeConfigItem.<Float>builder();
 
     public static RangeConfigItem.RangeConfigItemBuilder<Integer> INT(int defaultValue) {
         return INT.defaultValue(defaultValue);
@@ -15,5 +16,9 @@ public final class ConfigItems {
 
     public static RangeConfigItem.RangeConfigItemBuilder<Double> DOUBLE(double defaultValue) {
         return DOUBLE.defaultValue(defaultValue);
+    }
+
+    public static RangeConfigItem.RangeConfigItemBuilder<Float> FLOAT(float defaultValue) {
+        return FLOAT.defaultValue(defaultValue);
     }
 }
