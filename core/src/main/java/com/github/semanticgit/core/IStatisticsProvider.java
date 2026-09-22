86daa1b19d890aeadc840f0a22022038fd63cfd7
dynastxu <151742029+ETOofficial@users.noreply.github.com>
2026-09-22 16:32:31 +0800

    将 AbstractStatisticsProvider.java 改为接口

--- core/src/main/java/com/github/semanticgit/core/IStatisticsProvider.java
+++ core/src/main/java/com/github/semanticgit/core/IStatisticsProvider.java
@@ -15,10 +15,10 @@ import java.util.List;
  * 统计提供提供器抽象类。
  * @apiNote 所有数据获取应当默认懒加载，外键引用仅记录 id。
  */
-abstract class AbstractStatisticsProvider {
-    public abstract SimpleEntityChangeStatistics getSimpleEntityChangeStatistics();
+interface IStatisticsProvider {
+    SimpleEntityChangeStatistics getSimpleEntityChangeStatistics();
 
-    public abstract CommitEntityChangeStatistics getCommitEntityChangeStatistics(String hash);
+    CommitEntityChangeStatistics getCommitEntityChangeStatistics(String hash);
 
     /**
      * Get the entity change history.
@@ -26,7 +26,7 @@ abstract class AbstractStatisticsProvider {
      * @param refName    ref全名（如 "refs/heads/main"），从此ref的HEAD向父提交遍历
      * @return 实体变更历史记录
      */
-    public abstract EntityChangeHistory getEntityChangeHistory(String entityName, String refName);
+    EntityChangeHistory getEntityChangeHistory(String entityName, String refName);
 
     /**
      * Get the author change statistics.
@@ -34,19 +34,19 @@ abstract class AbstractStatisticsProvider {
      * @param refName ref 全名，为 null 时统计所有提交
      * @return 统计数据
      */
-    public abstract AuthorChangeStatistics getAuthorChangeStatistics(Author author, @Nullable String refName);
+    AuthorChangeStatistics getAuthorChangeStatistics(Author author, @Nullable String refName);
 
-    public abstract List<Author> getAuthors();
+    List<Author> getAuthors();
 
     /**
      * Get all entities.
      * @return 实体列表。
      */
-    public abstract List<Entity> getEntities();
+    List<Entity> getEntities();
 
     /**
      * Get all commits.
      * @return 提交元数据列表。
      */
-    public abstract List<CommitMeta> getCommits();
+    List<CommitMeta> getCommits();
 }
